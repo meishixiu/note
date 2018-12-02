fdd4a338c6fd507344925d72808240e0342e0e8f
meishixiu <meishixiu@outlook.com>
2018-12-03 04:59:25 +0800

    改一下注释

--- Aria2+AriaNg+Rclone+GoogleDrive/config.py
+++ Aria2+AriaNg+Rclone+GoogleDrive/config.py
@@ -5,7 +5,7 @@ def read():
                 "api": "http://127.0.0.1:6800/jsonrpc", # Aria2 RPC 地址
                 "token": "ec3c14e036ed3051e1d1",    # Aria2 RPC 令牌
                 "download_path": "/home/wwwroot/lixian.xxx.com/down/",  # Aruia2 下载目录 ( 结尾必须有 / )
-                "google_drive_path": "gd:Download/",    # Google Drive 上传目录 ( 如果是子目录，结尾必须有 / )
+                "google_drive_path": "gd:Download/",    # Google Drive 上传目录：前缀 gd 是配置 rclone 是取的名字；后缀是指定上传到 GD 的那个目录里 ( 如果指定目录，结尾必须有 / )
                 "filter_file": "/home/filter-file.txt", # rclone 上传文件夹时的过滤规则文件路径
                 "redis_host": "127.0.0.1",  # Redis 主机地址
                 "redis_port": 6379, # Redis 端口
